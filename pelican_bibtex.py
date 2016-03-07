ce2f6738836c5d9b526890d21f074f21bb23a0d9
Jacob Thebault-Spieker <thebault@cs.umn.edu>
2016-03-07 17:37:20 -0600

    make a download stats ,

--- pelican_bibtex.py
+++ pelican_bibtex.py
@@ -121,7 +121,7 @@ def add_publications(generator):
                              'note'    : get_field(entry, 'note'),
                              'year'   : entry.fields.get('year'),
                              'authorizer': get_field(entry, 'authorizer'),
-                             'acceptance': get_field(entry, 'acceptance')
+                             'acceptance': get_field(entry, 'acceptance'),
                              'stats': get_field(entry, 'stats')
                              })
 
